d7289f218542d5b08110535e2d94f1a8d00bc687
LeonarDev <leonardo.majevski@gmail.com>
2021-09-18 14:05:30 -0300

    init

---
+++ Extension-methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extension_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
